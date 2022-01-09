d452724b5ff10a9b905ee58b870753f5fa42c421
guravuppi <guravuppi987@gmail.com>
2022-01-09 12:57:09 +0530

    first commit

---
+++ mode1.py
@@ -0,0 +1,18 @@
+
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def pal(x):
+    x1=x[::-1]
+    if x==x1:
+        print('Palindrome')
+    else:
+        print('Not Palindrome')
+print(pal('madam'))
+        
